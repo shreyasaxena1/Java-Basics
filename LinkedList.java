1ea93f8105bc6d402d6b5e5b80f3c20979b7a579
Shreya Saxena <shreyasaxena117@gmail.com>
2022-01-28 16:50:49 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,20 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Myclass{
+    public static void main(String[] args){
+     Queue<Integer> q = new LinkedList<>();
+     q.offer(1); // can use q.add(1) but if operation does not complete then it will return exception but offer only return false 
+     q.offer(2);
+     q.offer(3);
+     
+     System.out.print(q);
+     
+     q.poll(); //can use q.remove() -- return exception if empty but pull return NULL
+     
+     System.out.print(q);
+     
+     System.out.print(q.element()); //can use q.peek() -- return exception if fails
+     
+    }
+}
