9bdc26a8d368ae4388380a279b7ed0161a01e347
Shreya Saxena <shreyasaxena117@gmail.com>
2022-01-30 20:20:13 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MyClass{
+    public static void main(String[] args){
+        System.out.println("Enter no:");
+        Scanner scn = new Scanner(System.in);
+        int s = scn.nextInt();
+        
+        System.out.print(s);
+    }
+}
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Integer: nextInt()
+Float: nextFloat()
+String : readLine()
+  
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
