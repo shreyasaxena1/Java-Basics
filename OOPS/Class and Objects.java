71e036b53cdf5f51a3e226367a82545874f3b7ba
Shreya Saxena <shreyasaxena117@gmail.com>
2022-01-31 01:07:19 +0530

    Create Class and Objects.java

---
+++ OOPS/Class and Objects.java
@@ -0,0 +1,13 @@
+public class Main{
+  public static void main(String[] args){
+      Person obj = new Person();
+      obj.name = " Shreya";
+      obj.age = 21;
+      System.out.println(obj.name +" "+ obj.age);
+  }   
+}
+
+class Person{
+    String name;
+    int age;
+}
