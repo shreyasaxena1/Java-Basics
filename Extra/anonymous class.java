bf70fa98a163fbf1f59ec48d661ce0d286bd6ef2
Shreya Saxena <shreyasaxena117@gmail.com>
2022-02-03 22:18:02 +0530

    Update and rename anonymous.java to anonymous class.java

--- Extra/anonymous class.java
+++ Extra/anonymous class.java
@@ -5,7 +5,7 @@ public class Main
 		    /*
 		    we want to use some other defination of method show 
 		    but don't want to override it using base class so
-		    we use anonymous class */
+		    we use anonymous class  -- one time use*/
 		    
 		    void show(){
 		    System.out.println("This is main's method");
