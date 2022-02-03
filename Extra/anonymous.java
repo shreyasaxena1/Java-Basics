5e3adc1a1f85044c9e83be74b8ed357a63accc85
Shreya Saxena <shreyasaxena117@gmail.com>
2022-02-03 22:16:09 +0530

    Create anonymous.java

---
+++ Extra/anonymous.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+		A obj = new A(){ 
+		    /*
+		    we want to use some other defination of method show 
+		    but don't want to override it using base class so
+		    we use anonymous class */
+		    
+		    void show(){
+		    System.out.println("This is main's method");
+		    }
+		};
+		
+		obj.show();
+	}
+}
+
+class A{
+    void show(){
+        System.out.println("This is A's method");
+    }
+}
